63c3941497b2318273be034cf3d5c17d55ff4a81
RFG\r00527175 <yuki.ito@jp.ricoh.com>
2024-11-04 18:06:12 +0900

    ch17

--- ch17/ex01/lint_sample.js
+++ ch17/ex01/lint_sample.js
@@ -1,10 +1,8 @@
-let a, x, y;
-const r = 10;
+let a, x, y
+const r = 10
 
-with (Math){
-  a = PI  * r * r;
-  x = r * cos(PI);
-  y = r * sin(PI / 2);
-}
+a = Math.PI * r * r
+x = r * Math.cos(Math.PI)
+y = r * Math.sin(Math.PI / 2)
 
-console.log(a, x, y);
+console.log(a, x, y)
